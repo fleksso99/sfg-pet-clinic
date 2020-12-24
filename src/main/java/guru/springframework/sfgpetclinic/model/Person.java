51cdd6f027cc8a00eb01680e50fcb7f722ce2676
Maxim <fleksso99@gmail.com>
2020-12-23 23:12:38 -0800

    init

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person {
+  private String firstName;
+  private String LastName;
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return LastName;
+  }
+
+  public void setLastName(String lastName) {
+    LastName = lastName;
+  }
+}
