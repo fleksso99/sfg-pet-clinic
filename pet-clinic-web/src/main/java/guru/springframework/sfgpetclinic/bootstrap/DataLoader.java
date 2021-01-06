ff08a67b139d573d5c3f9d6e0576625b2e5b99d5
Maxim <fleksso99@gmail.com>
2021-01-05 23:39:30 -0800

    update map impl

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -21,23 +21,26 @@ public class DataLoader implements CommandLineRunner {
   @Override
   public void run(String... args) {
     Owner owner1 = new Owner();
-    owner1.setId(1L);
     owner1.setFirstName("Maxim");
     owner1.setLastName("Tkac");
     ownerService.save(owner1);
 
     Owner owner2 = new Owner();
-    owner2.setId(2L);
     owner2.setFirstName("Ivan");
     owner2.setLastName("Petrov");
     ownerService.save(owner2);
 
     Vet vet1 = new Vet();
-    vet1.setId(1L);
     vet1.setFirstName("Sam");
     vet1.setLastName("Ivanov");
 
     vetService.save(vet1);
+
+    Vet vet2 = new Vet();
+    vet2.setFirstName("Samee");
+    vet2.setLastName("Ivanovww");
+
+    vetService.save(vet2);
 
 
   }
